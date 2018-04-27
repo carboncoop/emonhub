37eda28ab9b064f4f51ca240ea4aecdec719f043
Ben Aylott <ben@carbon.coop>
2018-04-27 16:05:14 +0100

    added support for device keys

--- src/interfacers/EmonHubEmoncmsHTTPInterfacer.py
+++ src/interfacers/EmonHubEmoncmsHTTPInterfacer.py
@@ -9,7 +9,7 @@ class EmonHubEmoncmsHTTPInterfacer(EmonHubInterfacer):
     def __init__(self, name):
         # Initialization
         super(EmonHubEmoncmsHTTPInterfacer, self).__init__(name)
-        
+
         # add or alter any default settings for this reporter
         # defaults previously defined in inherited emonhub_interfacer
         # here we are just changing the batchsize from 1 to 100
@@ -17,36 +17,49 @@ class EmonHubEmoncmsHTTPInterfacer(EmonHubInterfacer):
         self._defaults.update({'batchsize': 100,'interval': 30})
         # This line will stop the default values printing to logfile at start-up
         self._settings.update(self._defaults)
-        
+
         # interfacer specific settings
         self._cms_settings = {
             'apikey': "",
+            'devicekey':"",
             'url': "http://emoncms.org",
             'senddata': 1,
             'sendstatus': 0
         }
-        
+
         # set an absolute upper limit for number of items to process per post
         self._item_limit = 250
-        
+
         # maximum buffer size
         self.buffer._maximumEntriesInBuffer = 100000
-                    
+
     def _process_post(self, databuffer):
         """Send data to server."""
 
         # databuffer is of format:
         # [[timestamp, nodeid, datavalues][timestamp, nodeid, datavalues]]
         # [[1399980731, 10, 150, 250 ...]]
-        
+
+        apikey=True
+        devicekey=True
+
         if not 'apikey' in self._settings.keys() or str.__len__(str(self._settings['apikey'])) != 32 \
                 or str.lower(str(self._settings['apikey'])) == 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx':
+            apikey=False
+
+        if not 'devicekey' in self._settings.keys() or str.__len__(str(self._settings['devicekey'])) != 32 \
+                or str.lower(str(self._settings['devicekey'])) == 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx':
+            devicekey=False
+
+        if (not devicekey) and (not apikey):
             # Return true to clear buffer if the apikey is not set
             return True
-            
-            
+
+        if apikey and devicekey:
+            return True
+
         data_string = json.dumps(databuffer, separators=(',', ':'))
-        
+
         # Prepare URL string of the form
         # http://domain.tld/emoncms/input/bulk.json?apikey=12345
         # &data=[[0,10,82,23],[5,10,82,23],[10,10,82,23]]
@@ -56,14 +69,17 @@ class EmonHubEmoncmsHTTPInterfacer(EmonHubInterfacer):
         sentat = int(time.time())
 
         # Construct post_url (without apikey)
-        post_url = self._settings['url']+'/input/bulk'+'.json?apikey='
+        post_url = self._settings['url']+'/input/bulk'+'.json?'
         post_body = "data="+data_string+"&sentat="+str(sentat)
 
         # logged before apikey added for security
         self._log.info("sending: " + post_url + "E-M-O-N-C-M-S-A-P-I-K-E-Y&" + post_body)
 
         # Add apikey to post_url
-        post_url = post_url + self._settings['apikey']
+        if apikey:
+            post_url = post_url + 'apikey=' + self._settings['apikey']
+        elif devicekey:
+            post_url = post_url + 'devicekey=' + self._settings['devicekey']
 
         # The Develop branch of emoncms allows for the sending of the apikey in the post
         # body, this should be moved from the url to the body as soon as this is widely
@@ -76,13 +92,13 @@ class EmonHubEmoncmsHTTPInterfacer(EmonHubInterfacer):
         else:
             self._log.warning("send failure: wanted 'ok' but got '" +reply+ "'")
             return False
-            
-            
+
+
     def sendstatus(self):
         if not 'apikey' in self._settings.keys() or str.__len__(str(self._settings['apikey'])) != 32 \
                 or str.lower(str(self._settings['apikey'])) == 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx':
             return
-        
+
         # MYIP url
         post_url = self._settings['url']+'/myip/set.json?apikey='
         # Print info log
@@ -91,7 +107,7 @@ class EmonHubEmoncmsHTTPInterfacer(EmonHubInterfacer):
         post_url = post_url + self._settings['apikey']
         # send request
         reply = self._send_post(post_url,None)
-            
+
     def set(self, **kwargs):
         """
 
@@ -124,6 +140,23 @@ class EmonHubEmoncmsHTTPInterfacer(EmonHubInterfacer):
                     continue
                 self._settings[key] = setting
                 # Next line will log apikey if uncommented (privacy ?)
+                #self._log.debug(self.name + " apikey: " + str(setting))
+                continue
+            elif key == 'devicekey':
+                if str.lower(setting[:4]) == 'xxxx':
+                    self._log.warning("Setting " + self.name + " devicekey: obscured")
+                    pass
+                elif str.__len__(setting) == 32 :
+                    self._log.info("Setting " + self.name + " devicekey: set")
+                    pass
+                elif setting == "":
+                    self._log.info("Setting " + self.name + " devicekey: null")
+                    pass
+                else:
+                    self._log.warning("Setting " + self.name + " devicekey: invalid format")
+                    continue
+                self._settings[key] = setting
+                # Next line will log devicekey if uncommented (privacy ?)
                 #self._log.debug(self.name + " apikey: " + str(setting))
                 continue
             elif key == 'url' and setting[:4] == "http":
--- src/interfacers/__init__.py
+++ src/interfacers/__init__.py
@@ -5,7 +5,7 @@ __all__ = [
   "EmonHubPacketGenInterfacer",
   "EmonHubEmoncmsHTTPInterfacer",
   "EmonHubMqttInterfacer",
-  
+  "EmonHubKMPInterfacer",
   "EmonHubTx3eInterfacer",
   "EmonHubVEDirectInterfacer",
   # "EmonHubSmilicsInterfacer",
